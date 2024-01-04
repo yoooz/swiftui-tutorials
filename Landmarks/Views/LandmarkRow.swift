e524e3837f35b02d908cc613a07ea4cd50f90641
yoooz <boc.7zyzy@gmail.com>
2024-01-04 22:30:47 +0900

    Building lists and navigation: Section3

--- Landmarks/Views/LandmarkRow.swift
+++ Landmarks/Views/LandmarkRow.swift
@@ -16,12 +16,23 @@ struct LandmarkRow: View {
                 .resizable()
                 .frame(width: 50, height: 50)
             Text(landmark.name)
-            
+
             Spacer()
         }
     }
 }
 
-#Preview {
+#Preview("Turtle Rock") {
     LandmarkRow(landmark: landmarks[0])
+}
+
+#Preview("Salmon") {
+    LandmarkRow(landmark: landmarks[1])
+}
+
+#Preview("Group") {
+    Group {
+        LandmarkRow(landmark: landmarks[0])
+        LandmarkRow(landmark: landmarks[1])
+    }
 }
