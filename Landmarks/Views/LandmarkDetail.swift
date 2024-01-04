510883ffb9acb520ef620c02384e4e8f0f86cfd8
yoooz <boc.7zyzy@gmail.com>
2024-01-04 22:40:39 +0900

    Building lists and navigation: Section6

---
+++ Landmarks/Views/LandmarkDetail.swift
@@ -0,0 +1,47 @@
+//
+//  LandmarkDetail.swift
+//  Landmarks
+//
+//  Created by 荻原隆大 on 2024/01/04.
+//
+
+import SwiftUI
+
+struct LandmarkDetail: View {
+    var body: some View {
+        VStack {
+            MapView()
+                .frame(height: 300)
+
+            CircleImage()
+                .offset(y: -130)
+                .padding(.bottom, -130)
+
+            VStack(alignment: .leading) {
+                Text("Turtle Rock")
+                    .font(.title)
+
+                HStack {
+                    Text("Joshua Tree National Park")
+                    Spacer()
+                    Text("California")
+                }
+                .font(.subheadline)
+                .foregroundStyle(.secondary)
+
+                Divider()
+
+                Text("About Turtle Rock")
+                    .font(.title2)
+                Text("Descriptive text goes here.")
+            }
+            .padding()
+
+            Spacer()
+        }
+    }
+}
+
+#Preview {
+    LandmarkDetail()
+}
