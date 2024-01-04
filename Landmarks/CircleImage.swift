e0698d4d4b4f27b1ef2e4d3f58d6094e8256aaa4
yoooz <boc.7zyzy@gmail.com>
2024-01-04 18:15:19 +0900

    Section4

---
+++ Landmarks/CircleImage.swift
@@ -0,0 +1,23 @@
+//
+//  CircleImage.swift
+//  Landmarks
+//
+//  Created by 荻原隆大 on 2024/01/04.
+//
+
+import SwiftUI
+
+struct CircleImage: View {
+    var body: some View {
+        Image("turtlerock")
+            .clipShape(Circle())
+            .overlay {
+                Circle().stroke(.white, lineWidth: 4)
+            }
+            .shadow(radius: 7)
+    }
+}
+
+#Preview {
+    CircleImage()
+}
